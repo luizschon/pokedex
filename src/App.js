e3d8aea29ada0f4262e5a171446ec19285eb48c2
Luiz Schonarth <luizschon.unb2019@gmail.com>
2021-04-14 18:45:58 -0300

    Adiciona rotas, e listagem de pokemons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import * as Styled from './styles';
+import axios from "axios";
 
-export default App;
+import Page1 from './pages/Page1'
+import Page2 from './pages/Page2'
+
+export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const getPokemon = () => {
+    axios.get('https://pokedex20201.herokuapp.com/pokemons/')
+      .then(res => {
+        setPokemons(res.data.data.map(item => item))
+      })
+  }
+
+  useEffect(() => {
+    getPokemon();
+  }, [])
+
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+          <Route path="/">
+            <Page1 />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+
+      <Styled.Div>
+        {pokemons.map(item =>
+          <Styled.Grid key={item.id}>
+            <Styled.Item><img src={item.image_url} /></Styled.Item>
+            <Styled.Item><span>{item.number}</span></Styled.Item>
+            <Styled.Item><span>{item.name}</span></Styled.Item>
+          </Styled.Grid>)}
+      </Styled.Div>
+    </div>
+  );
+}
---
+++ src/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  font-family: "Lucida Console", "Courier New", monospace;
+  display: inline-block;
+  width: 20%;
+  height: 150px;
+`
+
+export const Div = styled.div`
+  justify-content: center;
+  padding: 30px;
+`
+
+export const Item = styled.div`
+  display: flex;
+  justify-content: center;
+`
