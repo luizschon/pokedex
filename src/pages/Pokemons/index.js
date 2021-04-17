c1b98a28708df8b6d817798088463c4a4bffa8fc
Luiz Schonarth <luizschon.unb2019@gmail.com>
2021-04-17 13:42:37 -0300

    Muda o nome da variável da página de pokemons

--- src/pages/Pokemons/index.js
+++ src/pages/Pokemons/index.js
@@ -1,46 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Switch, Link, Redirect, useParams } from 'react-router-dom';
-import * as Styled from './styles';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  useParams,
+} from "react-router-dom";
+import * as Styled from "./styles";
 import axios from "axios";
 
-import ShowPokemonInfo from './ShowPokemonInfo';
-
 const Pokemons = () => {
+  let { pageNumber } = useParams();
 
-  let { id } = useParams();
-
+  const [user, setUser] = useState(localStorage.getItem("user"));
   const [pokemons, setPokemons] = useState([]);
 
-  const getPokemon = (id) => {
-    setPokemons([])
-    axios.get('https://pokedex20201.herokuapp.com/pokemons?page=' + id)
-      .then(res => {
-        setPokemons(res.data.data.map(item => item))
-      })
-  }
+  const getPokemon = (pageNumber) => {
+    setPokemons([]);
+    axios
+      .get("https://pokedex20201.herokuapp.com/pokemons?page=" + pageNumber)
+      .then((res) => {
+        setPokemons(res.data.data.map((item) => item));
+      });
+  };
 
   useEffect(() => {
-    getPokemon(id);
-  }, [id])
+    getPokemon(pageNumber);
+  }, [pageNumber]);
 
-    return (
-    <Styled.Div>  
-      {pokemons.map(item =>
+  return (
+    <Styled.Div>
+      {pokemons.map((item) => (
         <Styled.Grid key={item.id}>
-          <Styled.Item><img src={item.image_url} alt={item.name}/></Styled.Item>
-          <Styled.Item><span>{item.number}</span></Styled.Item>
-          <Styled.Item><span>{item.name}</span></Styled.Item>
-          <br/>
-          <Styled.Item><Link to={{
-            pathname: `/pokemons/${item.name}`,
-            state: { pokemoninfo: item }
-          }}><Styled.Button>Info</Styled.Button></Link></Styled.Item>
-          <br/>
-        </Styled.Grid>)
-
-        }
+          <Styled.Item>
+            <img src={item.image_url} alt={item.name} />
+          </Styled.Item>
+          <Styled.Item>
+            <span>{item.number}</span>
+          </Styled.Item>
+          <Styled.Item>
+            <span>{item.name}</span>
+          </Styled.Item>
+          <br />
+          <Styled.Item>
+            <Link
+              to={{
+                pathname: `/pokemons/${item.name}`,
+                state: { pokemoninfo: item },
+              }}
+            >
+              <Styled.Button>Info</Styled.Button>
+            </Link>
+          </Styled.Item>
+          <br />
+        </Styled.Grid>
+      ))}
     </Styled.Div>
-    );
-}
+  );
+};
 
-export default Pokemons
+export default Pokemons;
