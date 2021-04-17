fd8177dc2a49c93fb04554c08bfa5314377000f3
Nathalia Pereira <nathalia.pereira252000@gmail.com>
2021-04-16 21:35:05 -0300

    Mostra informacoes dos pokemons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, useParams, Redirect} from 'react-router-dom';
 import Pokemons from './pages/Pokemons/index'
 
 export default function App() {
   return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/page">
-            <Pokemons />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-  );
-}
+        <BrowserRouter>
+          <div>
+                <Link to="/"></Link>
+                <Link to="/1">1</Link>
+                <Link to="/2">2</Link>
+                <Link to="/3">3</Link>
+                <Link to="/4">4</Link>
+                <Link to="/5">5</Link>
+                <Link to="/6">6</Link>
+                <Link to="/7">7</Link>
+                <Link to="/8">8</Link>
+                <Link to="/9">9</Link>
+                <Link to="/10">10</Link>
+                <Link to="/11">12</Link>
+                <Link to="/13">13</Link>
+                <Link to="/14">14</Link>
+                <Link to="/15">15</Link>
+                <Link to="/16">16</Link>
+                <Link to="/17">17</Link>
+                <Link to="/18">18</Link>
+                <Link to="/19">19</Link>
+                <Link to="/20">20</Link>
+                <Link to="/21">21</Link>
+                <Link to="/22">22</Link>
+                <Link to="/23">23</Link>
+                <Link to="/24">24</Link>
+                <Link to="/25">25</Link>
+                <Link to="/26">26</Link>
+                <Link to="/27">27</Link>
+                <Link to="/28">28</Link>
+                <Link to="/29">29</Link>
+                <Link to="/30">30</Link>
+                <Link to="/31">31</Link>
+                <Link to="/32">32</Link>
+                <Link to="/33">33</Link>
+
+            <Switch>
+            <Route exact path="/" render={() => {
+              return (
+                <Redirect to="/home" />
+              )
+             }}
+            />
+              <Route path="/:id" children={<Pokemons />} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      );
+    }
--- src/pages/Pokemons/index.js
+++ src/pages/Pokemons/index.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch, Link, useParams} from 'react-router-dom';
 import * as Styled from './styles';
 import axios from "axios";
 
 const Pokemons = () => {
+
+  let { id } = useParams();
+
   const [pokemons, setPokemons] = useState([]);
 
-  const getPokemon = () => {
-    axios.get('https://pokedex20201.herokuapp.com/pokemons')
+  const getPokemon = (id) => {
+    setPokemons([])
+    axios.get('https://pokedex20201.herokuapp.com/pokemons?page=' + id)
       .then(res => {
         setPokemons(res.data.data.map(item => item))
       })
   }
 
   useEffect(() => {
-    getPokemon();
-  }, [])
+    getPokemon(id);
+  }, [id])
 
   return (
-    <Styled.Div>
+    <Styled.Div>  
       {pokemons.map(item =>
         <Styled.Grid key={item.id}>
           <Styled.Item><img src={item.image_url} /></Styled.Item>
