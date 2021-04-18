afb8d1f9a347dd3d5933146366d2dcfc650f323f
Luiz Carlos Schonarth Junior <67851507+luizschonarth@users.noreply.github.com>
2021-04-17 22:14:45 -0300

    Deleta arquivo desnecessário Login.js
    A forma que index.js da página de login foi criada torna desnecessária a existência desse arquivo

--- src/components/Login.js
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import Input from "./Input";
-import Button from "./Button";
-
-const Login = ({ authenticateUser, createUser, error }) => {};
-
-export default Login;
