e29f8a388afe6a94f9a1299902047040f9d3f872
Luiz Schonarth <luizschon.unb2019@gmail.com>
2021-04-30 18:21:07 -0300

    Muda fonte global

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,13 +1,8 @@
 import { createGlobalStyle, css } from 'styled-components'
 
-const BarlowFont = css`
-  @import url('https://fonts.googleapis.com/css2?family=Barlow:ital@1&display=swap');
-`
-
 export const GlobalStyle = createGlobalStyle`
-  ${BarlowFont}
   body {
-    font: 1rem 'Barlow', sans-serif;
+    font-family: "Lucida Console", "Courier New", monospace;
     margin: 0px;
     background-color: white;
   }
